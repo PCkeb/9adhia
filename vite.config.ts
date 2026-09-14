a76d2d6d7e1aeaf364d4fb55774adbad4cc73bc1
PCkeb <abdrezakhaouari@gmail.com>
2026-09-14 19:56:14 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { fileURLToPath, URL } from 'node:url';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/9adhia/',
   plugins: [react()],
   resolve: {
     alias: {
